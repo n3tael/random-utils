a21f1858c58d1625f3844cab93f0e36399ad48ab
Bogdan <66169821+yanovsk1@users.noreply.github.com>
2022-02-19 17:40:29 +0200

    Update 0.0.1

---
+++ roflutils.js
@@ -0,0 +1,36 @@
+const package = require('./package.json')
+function sleep(ms) {return new Promise((resolve) => {setTimeout(resolve, ms);});}
+// Module functions
+function version() {return package.version;}
+async function deadinside() {
+    console.log('Я')
+    await sleep(1000);
+    console.log('Гуль...')
+    await sleep(1000);
+    for (i = 1000; i > 6; i++) {
+        i=i-7;
+        console.log(i+7+" - 7 = "+i);
+        await sleep(100);
+    }
+}
+function yesorno() {
+    var list = ['Да', 'Нет'];
+    console.log(list[Math.floor(Math.random()*list.length)])
+}
+async function deathgun() {
+    var list = ['дым', 'шелли с ультой', 'стена', 'deathgun', 'говно'];
+    console.log("советы от дезгана часть "+Math.floor(Math.random()*16384)+" 😈😈😈");
+    await sleep('1000');
+    console.log(list[Math.floor(Math.random()*list.length)]+" для нас корм.");
+    await sleep('2000');
+    console.log('ловим на ошибке 😈');
+    await sleep('1000');
+    console.log('убиваем...');
+    await sleep('5000');
+    console.log('идем плакать в лобби 😭😢')
+}
+// Export all functions
+module.exports.version = version; 
+module.exports.deadinside = deadinside; 
+module.exports.yesorno = yesorno;
+module.exports.deathgun = deathgun;
